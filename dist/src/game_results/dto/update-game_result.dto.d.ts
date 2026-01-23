6f765afea1c54ae51576e09ad9bffe9408e7de89
Tim Repke <repke@mcc-berlin.net>
2026-01-23 14:26:35 +0100

    fix gitignore

--- dist/src/game_results/dto/update-game_result.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class UpdateGameResultDto {
-    stoppingStep?: number;
-    score?: number;
-}
--- dist/src/users/dto/update-user.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class UpdateUserDto {
-    userName?: string;
-    email?: string;
-}
